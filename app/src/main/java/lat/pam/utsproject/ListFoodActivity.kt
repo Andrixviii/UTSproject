6088a1527b3e47b04a6c262730e68c0160bc8ca5
muhamad nopid andriansyah <muhamadnopidandriansyah@gmail.com>
2024-10-28 14:34:41 +0700

    Initial commit

---
+++ app/src/main/java/lat/pam/utsproject/ListFoodActivity.kt
@@ -0,0 +1,41 @@
+package lat.pam.utsproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class ListFoodActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: FoodAdapter
+    private lateinit var foodList: List<Food>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_list_food)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Menyiapkan data makanan
+        foodList = listOf(
+            Food("Cireng", "Cireng enak", R.drawable.cireng),
+            Food("Nasi Goreng JAWA", "Nasi Goreng asli jawa", R.drawable.nasigoreng),
+            Food("Kopi Ireng", "Kopi Kapal Api", R.drawable.kopi_hitam)
+        )
+
+        // Menginisialisasi adapter dengan context dan foodList
+        adapter = FoodAdapter(this, foodList)
+        recyclerView.adapter = adapter
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
